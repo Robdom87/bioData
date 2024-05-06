54813edffd15865c679a40656a2ab297fab04521
Cedar Chavez <geggas00@gmail.com>
2024-05-06 14:28:21 -0700

    reorg folder structure

---
+++ test.js
@@ -0,0 +1,25 @@
+{
+"Over_100k": "string",
+'category_id': "string",
+'comments_disabled': "string",
+'ratings_disabled': "string",
+'video_error_or_removed': "string"
+}
+["Days on Job", "Invoice Amt $", "AMT PAID"]
+
+["Days Out Classification","City Cleaned","Job Type Cleaned","PAID Y/N","Payment Plan","Insurance Y/N","Repeat Client","Collections","20k<"]
+
+"Month":"int64",
+"Days on Job":"int64",
+"Days from Completion to Invoice":"int64",
+"Days Out Classification":"string",
+"City Cleaned":"string",
+"Job Type Cleaned":"string",
+"Invoice Amt $":"string",
+"PAID Y/N":"string",
+"Payment Plan":"string",
+"AMT PAID":"string",
+"Insurance Y/N":"string",
+"Repeat Client":"string",
+"Collections":"string",
+"20k<":"string"
